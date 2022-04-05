d990f3ac15b80a0f1d18c30f066b5105159a7bb3
philliphnguyen <phillip.nguyen1997@yahoo.com>
2022-04-05 15:37:27 -0700

    worked on signup page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Layout from './components/Layout/Layout';
-import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App(){
@@ -14,6 +14,7 @@ function App(){
          <Switch>
              <Route exact path="/" component={Layout}/>
              <Route exact path="/login" component={Login}/>
+             <Route exact path="/signup" component={SignUp}/>
          </Switch>
          </Router>
         </div>
