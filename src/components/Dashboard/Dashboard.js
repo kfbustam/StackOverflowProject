586b2965d8eb66264b9bb55c7dd2f0b015fcdec6
Sai Manasa Yadlapalli <manasa.yadlapalli5@gmail.com>
2022-04-04 20:26:23 -0700

    [Frontend] Created static Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,12 @@
-// import React from 'react';
-// import './Dashboard.css';
+import React from 'react';
+import './Dashboard.css';
 
-// function Dashboard() {
-//   return (
-//     <h1>
-//       Dashboard
-//     </h1>
-//   )
-// }
+function Dashboard() {
+  return (
+    <h1>
+      Dashboard
+    </h1>
+  )
+}
 
-// export default Dashboard
+export default Dashboard
