2f4fa8e5011d4dfa8e63d582d33d599c2fbafd02
philliphnguyen <phillip.nguyen1997@yahoo.com>
2022-04-25 17:51:24 -0700

    display vote and answer number

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,6 +5,37 @@ import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import './Search.css'
 const Search = () => {
+    const questions = [{
+        votes: 5,
+        answers: 3,
+        title: 'Question title',
+        description: 'Description of the problem',
+        answered: false,
+        tags: ['java', 'python', 'javascript'],
+        user: 'Phillip Nguyen',
+        date: new Date()
+    },
+    {
+        votes: 5,
+        answers: 3,
+        title: 'Question title',
+        description: 'Description of the problem',
+        answered: true,
+        tags: ['java', 'python', 'javascript'],
+        user: 'Phillip Nguyen',
+        date: new Date()
+    },
+    {
+        votes: 5,
+        answers: 3,
+        title: 'Question title',
+        description: 'Description of the problem',
+        answered: false,
+        tags: ['java', 'python', 'javascript'],
+        user: 'Phillip Nguyen',
+        date: new Date()
+    }]
+
     return (
         <div className='search-container'>
             <Container className='mt-3 search-head-container'>
@@ -12,7 +43,7 @@ const Search = () => {
                     <Col md={8}>
                         <h3>Search results</h3>
                     </Col>
-                    <Col md={4} style={{display: 'flex', justifyContent: 'right'}}>
+                    <Col md={4} style={{ display: 'flex', justifyContent: 'right' }}>
                         <Button className='ask-question-button'>Ask question</Button>
                     </Col>
                 </Row>
@@ -30,9 +61,25 @@ const Search = () => {
                         </ButtonGroup>
                     </Col>
                 </Row>
-                
             </Container>
-            <hr className='search-hr'/>
+            <hr className='search-hr' />
+            {questions.map(question => (
+                <div>
+                    <Container>
+                        <Row>
+                            <Col md={2} className='text-center' style={{width: '80px'}}>
+                                <strong className='vote-number'>{question.votes}</strong>
+                                <p className='vote-text'>votes</p>
+                                <div className={question.answered ? 'answered' : null}>
+                                    <strong className={question.answered ? 'answer-number' : 'unanswered-number'}>{question.answers}</strong>
+                                    <p className={question.answered ? 'answer-text' : 'unanswered-text'}>answers</p>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <hr className='search-hr' />
+                </div>
+            ))}
         </div>
     )
 }
