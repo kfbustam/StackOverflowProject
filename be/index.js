93418ee6942d4af30f17d117e78c55a147135c48
philliphnguyen <phillip.nguyen1997@yahoo.com>
2022-05-02 19:22:52 -0700

    updated register api to use username

--- be/index.js
+++ be/index.js
@@ -57,7 +57,7 @@ app.use('/api/user',user)
 
 
 // server listening 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
