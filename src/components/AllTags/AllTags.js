d69616dfa95d9974e7d2f520def4df504aa5ecd3
philliphnguyen <phillip.nguyen1997@yahoo.com>
2022-05-04 21:06:04 -0700

    tag page done w/ changes to suggestions

--- src/components/AllTags/AllTags.js
+++ src/components/AllTags/AllTags.js
@@ -8,25 +8,43 @@ import Col from 'react-bootstrap/Col'
 function AllTags() {
   const [tags, setTags] = useState([{}])
   const [filteredTags, setFilteredTags] = useState([{}])
+  const [tagNames, setTagNames] = useState([])
+  const [searchText, setSearchText] = useState('')
 
   useEffect(() => {
     const data = []
 
     for (let i = 0; i < 10; i++) {
-      data.push({
-        name: 'java',
-        description: `Java is a high-level object oriented programming language. Use this tag when you're 
-        having problems using or understanding the language itself. This tag is frequently 
-        used alongside other tags for libraries and/or frameworks used by Java developers.`,
-        count: 1842957,
-        todaycount: 387,
-        weekcount: 2253
+        data.push({
+          name: 'java',
+          description: `Java is a high-level object oriented programming language. Use this tag when you're 
+          having problems using or understanding the language itself. This tag is frequently 
+          used alongside other tags for libraries and/or frameworks used by Java developers.`,
+          count: 1842957,
+          todaycount: 387,
+          weekcount: 2253
+          
       })
-
-      setTags(data)
-      setFilteredTags(data)
     }
+
+    data[4].name = 'javascript'
+    data[7].name = 'php'
+
+    const nameData = data.map(tag => tag.name)
+
+    setTags(data)
+    setFilteredTags(data)
+    setTagNames(nameData)
   }, [])
+
+  const handleSearch = (text) => {
+    setSearchText(text)
+    let allTags = tags
+
+    allTags = allTags.filter(tag => tag.name.toLowerCase().includes(text.toString().toLowerCase()))
+
+    setFilteredTags(allTags)
+  }
 
   return (
     <Container className='all-tags-container mt-4'>
@@ -35,8 +53,9 @@ function AllTags() {
         <br />
         Using the right tags makes it easier for others to find and answer your question.
       </p>
-      <Typeahead id='all-tags-input' options={[]} placeholder='Filter by tag name' className='w-25' maxResults={5}
-      />
+      <Typeahead id='all-tags-input' options={tagNames} placeholder='Filter by tag name' className='w-25' maxResults={5}
+        onInputChange={(text) => handleSearch(text)} onChange={(text) => handleSearch(text)}
+        open={searchText.length >= 3}/>
       <Row className='mt-5'>
         {filteredTags.map(tag => (
           <Col sm={6} md={4} lg={3} className='mb-3'>
