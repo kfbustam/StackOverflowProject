cb2801aa3a045fa686e00c3ef2c2de25fa64d203
sriram <36560070+chintasriram@users.noreply.github.com>
2022-05-09 16:51:43 -0700

    updated messaging API's

--- be/services/message.js
+++ be/services/message.js
@@ -6,86 +6,99 @@ class Message {
 
     static createConversation = async (data) => {
         try {
-            const newConversation = new ConversationModel({
-                members: [data.sender, data.receiver],
-              });
-            
+            let result={};
+            const isExists = await ConversationModel.find({
+                $and: [
+                    { members: { $in: data.sender } },
+                    { members: { $in: data.receiver } }
+                ]
+            });
+            if (isExists?.length) {
+                result.isExists=true;
+                result.error="Conversation already present";
+                result.conversation = isExists[0]._id;
+                return result
+            }
+            else {
+                const newConversation = new ConversationModel({
+                    members: [data.sender, data.receiver],
+                });
+
                 const savedConversation = await newConversation.save();
-               if(savedConversation)
-               {
-                   return savedConversation;
-               }
-               else return []
 
-         }
-         catch(err){
-                 console.log(err);
-                 console.log("Some unexpected error while creating a conversation")
-         }
+                if (savedConversation) {
+                    result.isExists=false;
+                    result.savedConversation=savedConversation;
+                    return result;
+                }
+                else return []
+            }
+        }
+        catch (err) {
+            console.log(err);
+            console.log("Some unexpected error while creating a conversation")
+        }
 
-}
+    }
 
-static getAllUserConversations = async (data) => {
-    try {
-        const conversations = await ConversationModel.find({
-            members: { $in: [data] },
-          });
+    static getAllUserConversations = async (data) => {
+        try {
+            const conversations = await ConversationModel.find({
+                members: { $in: [data] },
+            });
 
-           if(conversations)
-           {
-               return conversations;
-           }
-           else return []
+            if (conversations) {
+                return conversations;
+            }
+            else return []
 
-     }
-     catch(err){
-             console.log(err);
-             console.log("Some unexpected error while creating a conversation")
-     }
-}
+        }
+        catch (err) {
+            console.log(err);
+            console.log("Some unexpected error while creating a conversation")
+        }
+    }
 
 
-static addMessageToConversation = async (data) => {
-    try {
-        const newMessage = new MessageModel({
-            conversationId: data.conversationId,
-            sender:data.sender,
-            message:data.message
-          });
+    static addMessageToConversation = async (data) => {
+        try {
+            const newMessage = new MessageModel({
+                conversationId: data.conversationId,
+                sender: data.sender,
+                message: data.message
+            });
 
-          const saveMessage = await newMessage.save();
+            const saveMessage = await newMessage.save();
 
-           if(saveMessage)
-           {
-               return saveMessage;
-           }
-           else return []
+            if (saveMessage) {
+                return saveMessage;
+            }
+            else return []
 
-     }
-     catch(err){
-             console.log(err);
-             console.log("Some unexpected error while creating a conversation")
-     }
-}
+        }
+        catch (err) {
+            console.log(err);
+            console.log("Some unexpected error while creating a conversation")
+        }
+    }
 
-static getAllMessagesOfConversation = async (data) => {
-    try {
-        const messages = await MessageModel.find({
-            conversationId: data,
-          });
+    static getAllMessagesOfConversation = async (data) => {
+        try {
+            const messages = await MessageModel.find({
+                conversationId: data,
+            });
 
-           if(messages)
-           {
-               return messages;
-           }
-           else return []
+            if (messages) {
+                return messages;
+            }
+            else return []
 
-     }
-     catch(err){
-             console.log(err);
-             console.log("Some unexpected error while getting messages of a conversation")
-     }
-}
+        }
+        catch (err) {
+            console.log(err);
+            console.log("Some unexpected error while getting messages of a conversation")
+        }
+    }
 
 }
 
