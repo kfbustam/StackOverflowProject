5c4d76398ad2978b2839c598f496c58d2aa2cb1d
Suriya Palanikumar <suriya.palanikumar@sjsu.edu>
2022-05-09 17:39:38 -0700

    Tags Tab under activity tab

--- be/controllers/user.js
+++ be/controllers/user.js
@@ -320,5 +320,31 @@ router.get("/getQuestionsTab/:id", async (req, res) => {
     }
 })
 
+router.get("/getTagsTab/:id", async (req, res) => {
+    const response={} 
+    try{
+        //console.log(userId)
+        const result = await User.getTagsTab(req.params.id);          
+        if(result){
+            response.success = true;
+            response.user = result;
+            response.status = "200";
+            res.status(200).send(response);
+
+        }else{
+            response.success = false;
+            response.error = "Cannot get the questions tab of the user";
+            response.status = "400";
+            res.status(400).send(response);
+        }
+    }catch(e){
+        console.log(e);
+        response.success = false;
+        response.error = "Some error occurred. Please try again later";
+        response.status = "500";
+        res.status(500).send(response);
+    }
+})
+
 
 module.exports = router;
